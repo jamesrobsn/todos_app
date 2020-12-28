70dbfb56eaad7d45e1e85ba6cf15c3b142761431
James Robinson <jamesrobsn@hotmail.com>
2020-12-28 13:48:06 -0600

    fixed .gitignore

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
